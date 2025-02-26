b9fe21d828b0382bc19bb2f37756bf730189ee13
amanraval76593 <aman76593@gmail.com>
2025-02-26 20:08:38 +0530

    added basic templates and models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(
+        default=timezone.now
+    )  # default value for date_posted will be set to current time for the first time of post creation.
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )  # Instead of storing all user detail for every post forieng key store reference to the user and on_delete specify to delete the current post if user is deleted.
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+posts = [
+    {
+        "title": "Blog post 1",
+        "author": "Aman",
+        "date_posted": "August 12",
+        "content": "This is the first post",
+    },
+    {
+        "title": "Blog post 2",
+        "author": "ARaval",
+        "date_posted": "August 12",
+        "content": "This is the Second post",
+    },
+]
+
+
+def home(request):
+    context = {
+        "posts": Post.objects.all(),
+    }
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "About"})
